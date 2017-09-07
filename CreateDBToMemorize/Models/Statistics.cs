bc05e8fc307398435df430f58e67fdae248ce945
Nazar <nazariy84@gmail.com>
2017-09-07 13:41:44 +0300

    add classes

---
+++ CreateDBToMemorize/Models/Statistics.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CreateDBToMemorize.Models
+{
+    public class Statistics
+    {
+        public int SuccessPercent { get; set; }
+        public User User { get; set; }
+        public Deck Deck { get; set; }
+    }
+}
