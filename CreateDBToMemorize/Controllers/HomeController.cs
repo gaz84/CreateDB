2e803e7d7a664ccd7903be6cb97d58e6e2c155bb
Nazar <nazariy84@gmail.com>
2017-09-10 13:39:58 +0300

    add DAL

--- CreateDBToMemorize/Controllers/HomeController.cs
+++ CreateDBToMemorize/Controllers/HomeController.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MemoDAL;
+using MemoDAL.EF;
+using MemoDAL.Entities;
 
 namespace CreateDBToMemorize.Controllers
 {
@@ -11,7 +14,14 @@ namespace CreateDBToMemorize.Controllers
         public ActionResult Index()
         {
             ViewBag.Title = "Home Page";
+            using (UnitOfWork un = new UnitOfWork(new MemoContext()))
+            {
+                Role r = new Role { Name = "Test" };
+                un.Roles.Create(r);
 
+                var roles = un.Roles.GetAll();
+                un.Save();
+            }
             return View();
         }
     }
